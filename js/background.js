90658fb67688cf159eb78c4a6b66405451831223
Sean Bucholtz <sean.bucholtz@expeditors.com>
2017-11-29 09:07:01 -0800

    In the depths of callback hell

--- js/background.js
+++ js/background.js
@@ -1,17 +1,40 @@
-var context = "editable";
-var contextID = "enableAutoComplete";
-var title = "Auto-complete for this element";
+// function setElementClicked(elementClicked) {
+//     setElementClickedID(elementClicked["ID"]);
+//     setElementClickedPageURL(elementClicked["URL"]);
+// }
 
-var elementClickedID = null;
-var elementClickedPageURL = null;
-
-function setElementClicked(elementClicked) {
-    elementClickedID = elementClicked["ID"];
-    elementClickedPageURL = elementClicked["URL"];
-}
-
+// function getElementClickedID() {
+//     var elementClickedID = null;
+//     chrome.storage.local.get('elementClickedID', function(result) {
+//         elementClickedID = result.elementClickedID;
+//         console.log("retrieved locally elementClickedID as: " + elementClickedID);
+//     });
+//     return elementClickedID;
+// }
+//
+// function setElementClickedID(elementClickedID) {
+//     chrome.storage.local.set({'elementClickedID': elementClickedID}, function() {
+//         console.log("saved locally elementClickedID as: " + elementClickedID);
+//     })
+// }
+//
+// function getElementClickedPageURL() {
+//     var elementClickedPageURL = null;
+//     chrome.storage.local.get('elementClickedPageURL', function(result) {
+//         elementClickedPageURL = result.elementClickedPageURL;
+//         console.log("retrieved locally elementClickedPageURL as: " + elementClickedPageURL);
+//     });
+//     return elementClickedPageURL;
+// }
+//
+// function setElementClickedPageURL(elementClickedPageURL) {
+//     chrome.storage.local.set({'elementClickedPageURL': elementClickedPageURL}, function() {
+//         console.log("saved locally elementClickedPageURL as: " + elementClickedPageURL);
+//     })
+// }
 
 chrome.runtime.onInstalled.addListener(function() {
+    var context = "editable";
     chrome.contextMenus.create({"title": "On", "contexts": [context], "type": "radio",
         "id": "enable"});
     chrome.contextMenus.create({"title": "Off", "contexts": [context], "type": "radio",
@@ -20,61 +43,65 @@ chrome.runtime.onInstalled.addListener(function() {
 
 
 function onClickHandler(info, tab) {
-    // debugger;
 
     console.log("item " + info.menuItemId + " was clicked");
     console.log("info: " + JSON.stringify(info));
     console.log("tab: " + JSON.stringify(tab));
+    debugger;
+    // chrome.tabs.sendMessage(tab.id, "getElementClicked", setElementClicked);
+    // var elementClickedID = getElementClickedID();
+    // var elementClickedPageURL = getElementClickedPageURL();
 
     var menuItem = info.menuItemId;
-    if(menuItem === "enable") {
-        chrome.tabs.sendMessage(tab.id, "enable", setElementClicked);
-        console.log(elementClickedID);
-        if(!trackingID(elementClickedID)) {
-            saveID(elementClickedID, elementClickedPageURL);
-        }
-    }
-    else if(menuItem === "disable") {
-        if(trackingID(elementClickedID)) {
-            deleteID(elementClickedID, elementClickedPageURL);
-        }
-    }
+    chrome.tabs.sendMessage(tab.id, {greeting: menuItem}, function(response) {
+        console.log(menuItem + "d " + response.farewell);
+    });
+    // if(menuItem === "enable") {
+    //     chrome.tabs.sendMessage(tab.id, {greeting: "enable"}, function(response) {
+    //         console.log("Enabled " + response.farewell);
+    //     });
+    //     // chrome.tabs.sendMessage({active: true, currentWindow: true}, function (tabs)
+    //     // {
+    //     //     chrome.tabs.sendMessage(tabs[0].id, {greeting: "enable"}, function(response) {
+    //     //         console.log("Enabled " + response.farewell);
+    //     //     });
+    //     // });
+    // }
+    // else if(menuItem === "disable") {
+    //     chrome.tabs.sendMessage(tab.id, {greeting: "enable"}, function(response) {
+    //         console.log("Enabled " + response.farewell);
+    //     });
+    //     // chrome.tabs.sendMessage({active: true, currentWindow: true}, function (tabs)
+    //     // {
+    //     //     chrome.tabs.sendMessage(tabs[0].id, {greeting: "disable"}, function(response) {
+    //     //         console.log("Disabled " + response.farewell);
+    //     //     });
+    //     // });
+    // }
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 
-function logStuff(elementID) {
-    alert("I received the following DOM content:\n" + elementID);
-    console.log("I received the following DOM content:\n" + elementID);
-}
+// function trackingID(URL, ID) {
+//     console.log(URL);
+//     var urlArray = queryStorageForURL(URL);
+//     if(!urlArray) {
+//         return false;
+//     }
+//     else {
+//         return $.inArray(urlArray, ID) > -1;
+//     }
+// }
 
-//document.addEventListener('DOMContentLoaded', function() {
-//    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-//        var url = tabs[0].url;
-//        console.log(url);
-//    });
-//}, false);
-
-function trackingID(URL, ID) {
-    console.log(URL);
-    var urlArray = queryStorageForURL(URL);
-    if(!urlArray) {
-        return false;
-    }
-    else {
-        return $.inArray(urlArray, ID) > -1;
-    }
-}
-
-function queryStorageForURL(URL) {
-    console.log(URL);
-    chrome.storage.sync.get([URL], function(result) {
-        debugger;
-        console.log("reslt: " + JSON.stringify(result));
-        return result[pageURL] ? result[pageURL] : null;
-    });
-}
+// function queryStorageForURL(URL) {
+//     console.log(URL);
+//     chrome.storage.sync.get([URL], function(result) {
+//         debugger;
+//         console.log("reslt: " + JSON.stringify(result));
+//         return result[pageURL] ? result[pageURL] : null;
+//     });
+// }
 
 //function saveSetting(setting) {
 //    var pageURL = setting.URL;
@@ -84,18 +111,18 @@ function queryStorageForURL(URL) {
 //    saveID(pageURL, elementClickedID);
 //}
 
-function saveID(ID, pageURL) {
-    debugger;
-    console.log("save ID: pageURL = "+ pageURL + " ID = " + ID);
-    var urlArray = queryStorageForURL(pageURL);
-    if($.inArray(urlArray, ID) < 0) {
-        urlArray.unshift(ID);
-        var jsonObj = {};
-        jsonObj[pageURL] = urlArray;
-        chrome.storage.sync.set(jsonObj, function() {
-            console.log("Saved ID: " + ID + " under URL: " + pageURL);
-        });
-    }
+// function saveID(ID, pageURL) {
+//     debugger;
+//     console.log("save ID: pageURL = "+ pageURL + " ID = " + ID);
+//     var urlArray = queryStorageForURL(pageURL);
+//     if($.inArray(urlArray, ID) < 0) {
+//         urlArray.unshift(ID);
+//         var jsonObj = {};
+//         jsonObj[pageURL] = urlArray;
+//         chrome.storage.sync.set(jsonObj, function() {
+//             console.log("Saved ID: " + ID + " under URL: " + pageURL);
+//         });
+//     }
 //    chrome.storage.sync.get([pageURL], function(result) {
 //        debugger;
 //        console.log("reslt: " + JSON.stringify(result));
@@ -109,22 +136,22 @@ function saveID(ID, pageURL) {
 //            });
 //        }
 //    });
-}
+// }
 
-function deleteID(ID, pageURL) {
-    debugger;
-    console.log("save ID: pageURL = "+ pageURL + " ID = " + ID);
-    var urlArray = queryStorageForURL(pageURL);
-    var index = $.inArray(urlArray, ID);
-    if(index > -1) {
-        urlArray = $.grep(urlArray, function(value) {
-            return value !== ID;
-        });
-        var jsonObj = {};
-        jsonObj[pageURL] = urlArray;
-        chrome.storage.sync.set(jsonObj, function() {
-            console.log("re ID: " + ID + " under URL: " + pageURL);
-        });
-    }
-}
+// function deleteID(ID, pageURL) {
+//     debugger;
+//     console.log("save ID: pageURL = "+ pageURL + " ID = " + ID);
+//     var urlArray = queryStorageForURL(pageURL);
+//     var index = $.inArray(urlArray, ID);
+//     if(index > -1) {
+//         urlArray = $.grep(urlArray, function(value) {
+//             return value !== ID;
+//         });
+//         var jsonObj = {};
+//         jsonObj[pageURL] = urlArray;
+//         chrome.storage.sync.set(jsonObj, function() {
+//             console.log("re ID: " + ID + " under URL: " + pageURL);
+//         });
+//     }
+// }
 
--- js/content.js
+++ js/content.js
@@ -1,6 +1,10 @@
-var lastClicked = null;
-var pageURL = null;
+$(document).ready(function() {
+    var pageURL = window.location.href;
+    setElementClickedPageURL(pageURL);
+    queryStorageForURL(enableAutoCompleteOnFields, pageURL);
+});
 
+//------------------------------------- Proxy Script ------------------------------------------------//
 var mouseClickProxy = document.createElement('script');
 mouseClickProxy.id = 'mouseClickProxyID';
 mouseClickProxy.dataset.elementClicked = null;
@@ -19,58 +23,176 @@ mouseClickProxy.textContent = [
 
 document.documentElement.appendChild(mouseClickProxy);
 
+//------------------------------------- Proxy Script ------------------------------------------------//
+
+//--------------------------------Proxy Script Data Listener-----------------------------------------//
 var lastElementClickedListener = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         if((mutation.type === 'attributes') && (mutation.attributeName === 'data-element-clicked')) {
             console.log("Overwriting last clicked..");
-            lastClicked = mouseClickProxy.dataset.elementClicked;
-            console.log('Mouse event lastClicked = ' + lastClicked);
+            setElementClickedID(mouseClickProxy.dataset.elementClicked);
         }
     });
 });
 
 lastElementClickedListener.observe(mouseClickProxy, {attributes: true});
+//--------------------------------Proxy Script Data Listener-----------------------------------------//
 
+//---------------------------------- Background Listener --------------------------------------------//
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if(request === "getElementClicked") {
-        sendResponse(lastClicked);
+    // console.log("Message from background:" + sender.tab.url);
+    debugger;
+    // var elementID = getElementClickedID();
+    if(request.greeting === "enable") {
+        getElementClickedID(enableAutoComplete);
+        // enableAutoComplete(elementID);
+        // if(!trackingID(elementID)) {
+        //     saveID(elementID, getElementClickedPageURL());
+        // }
     }
-    if(request === "enable") {
-        console.log("enabling auto-complete on: " + lastClicked);
-        enableAutoComplete(lastClicked);
-//        console.log("Saving " + lastClicked + " under URL " + pageURL);
-        sendResponse({"URL": pageURL, "ID": lastClicked});
+    else if(request.greeting === "disable") {
+        getElementClickedID(disableAutoComplete);
+        // disableAutoComplete(elementID);
+        // if(trackingID(elementID)) {
+        //     deleteID(elementID, getElementClickedPageURL());
+        // }
     }
-}, false);
+    sendResponse({farewell: "test"});
+});
+//---------------------------------- Background Listener --------------------------------------------//
 
+//-------------------------------------- Workers ----------------------------------------------------//
 
 function enableAutoComplete(elementID) {
+    console.log("enabling auto-complete on: " + elementID);
+    toggleAutoComplete("ON", elementID);
+    getElementClickedPageURL(saveID, elementID);
+    // if(!trackingID(elementID)) {
+    //     getElementClickedPageURL(saveID, elementID);
+    //     // saveID(elementID, getElementClickedPageURL);
+    // }
+}
+
+function enableAutoCompleteOnFields(savedFields) {
+    if(savedFields !== undefined) {
+        $.each(savedFields, function (index, field) {
+            enableAutoComplete(field);
+        })
+    }
+}
+
+function disableAutoComplete(elementID) {
+    console.log("disabling auto-complete on: " + elementID);
+    toggleAutoComplete("OFF", elementID);
+    getElementClickedPageURL(deleteID, elementID);
+    // if(trackingID(elementID)) {
+    //     getElementClickedPageURL(deleteID, elementID);
+    //     // deleteID(elementID, getElementClickedPageURL());
+    // }
+}
+
+function toggleAutoComplete(ON_OFF, elementID) {
+    // debugger;
+    ON_OFF = ON_OFF.toLowerCase();
+    if(ON_OFF !== "on" && ON_OFF !== "off") {
+        return false;
+    }
+    $('#' + elementID).attr("autocomplete", ON_OFF);
+}
+
+// function trackingID(URL, ID) {
+//     console.log(URL);
+//     var urlArray = queryStorageForURL(URL);
+//     if(!urlArray) {
+//         return false;
+//     }
+//     else {
+//         return $.inArray(urlArray, ID) > -1;
+//     }
+// }
+//-------------------------------------- Workers ----------------------------------------------------//
+
+//------------------------------------ Sync Storage ------------------------------------------------//
+function queryStorageForURL(callback, pageURL) {
+    console.log("queryStorageForURL: " + pageURL);
+    chrome.storage.sync.get(pageURL, function(result) {
+        debugger;
+        console.log("result: " + JSON.stringify(result));
+        result = result[pageURL] ? result[pageURL] : undefined;
+        callback(result, arguments);
+    });
+}
+
+function saveID(ID, getPageURL) {
     debugger;
-    console.log('elementID: ' + elementID);
-    if(elementID === undefined || elementID === null) {
-        console.log('ID is null: unable to enable auto-complete on this element');
-        return false;
-    }
-    var inputElement = $('#' + elementID);
-    if(hasAutoCompleteDisabled(inputElement)) {
-        inputElement.setAttribute("autocomplete", "on")
+    var pageURL = getPageURL();
+    console.log("saveID: pageURL = "+ pageURL + " ID = " + ID);
+    queryStorageForURL(addIdToURLArray, pageURL, ID);
+}
+
+function addIdToURLArray(urlArray, ID) {
+    debugger;
+    ID = arguments[1];
+    if($.inArray(urlArray, ID) < 0) {
+        urlArray.unshift(ID);
+        var jsonObj = {};
+        jsonObj[pageURL] = urlArray;
+        chrome.storage.sync.set(jsonObj, function() {
+            console.log("Saved ID: " + ID + " under URL: " + pageURL);
+        });
     }
 }
 
-function hasAutoCompleteDisabled(element) {
-    var autoComplete = $(element).attr("autocomplete");
-    if(!autoComplete) {
-        return false;
-    }
-    else if(autoComplete === "off") {
-        return true;
-    }
-    else {
-        return false;
+function deleteID(ID, pageURL) {
+    // debugger;
+    console.log("save ID: pageURL = "+ pageURL + " ID = " + ID);
+    var urlArray = queryStorageForURL(pageURL);
+    var index = $.inArray(urlArray, ID);
+    if(index > -1) {
+        urlArray = $.grep(urlArray, function(value) {
+            return value !== ID;
+        });
+        var jsonObj = {};
+        jsonObj[pageURL] = urlArray;
+        chrome.storage.sync.set(jsonObj, function() {
+            console.log("re ID: " + ID + " under URL: " + pageURL);
+        });
     }
 }
+//------------------------------------ Sync Storage ------------------------------------------------//
 
-$(document).ready(function() {
-    pageURL = window.location.href;
-});
+//------------------------------------ Local Storage ----------------------------------------------//
+function getElementClickedID(callback) {
+    // debugger;
+    chrome.storage.local.get('elementClickedID', function(result) {
+        debugger;
+        var elementClickedID = result['elementClickedID'];
+        console.log("retrieved locally elementClickedID as: " + elementClickedID);
+        callback(elementClickedID);
+    });
+}
 
+function setElementClickedID(elementClickedID) {
+    // debugger;
+    chrome.storage.local.set({'elementClickedID': elementClickedID}, function() {
+        console.log("saved locally elementClickedID as: " + elementClickedID);
+    })
+}
+
+function getElementClickedPageURL(elementClickedID, callback) {
+    // debugger;
+    chrome.storage.local.get('elementClickedPageURL', function(result) {
+        debugger;
+        var elementClickedPageURL = result['elementClickedPageURL'];
+        console.log("retrieved locally elementClickedPageURL as: " + elementClickedPageURL);
+        callback(elementClickedID, elementClickedPageURL);
+    });
+}
+
+function setElementClickedPageURL(elementClickedPageURL) {
+    // debugger;
+    chrome.storage.local.set({'elementClickedPageURL': elementClickedPageURL}, function() {
+        console.log("saved locally elementClickedPageURL as: " + elementClickedPageURL);
+    })
+}
+//------------------------------------ Local Storage ----------------------------------------------//
